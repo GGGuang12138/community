615d3c439cc6db89c315f91e5c1582fb1d443a8b
ggguang <gguang12138@icloud.com>
2020-02-02 22:14:36 +0800

    add index html

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## 资料
 https://spring.io/guides
+https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
 
 
 ## 工具
--- src/main/java/vip/gg/community/demo/controller/HelloController.java
+++ src/main/java/vip/gg/community/demo/controller/HelloController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
