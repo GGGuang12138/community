4ae5c5bbedb3566efbe45ee54863649e12d2cc6e
Gg <ggguang@GgdeMacBook-Air.local>
2020-02-02 17:40:21 +0800

    init repo

---
+++ src/test/java/vip/gg/community/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.gg.community.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
